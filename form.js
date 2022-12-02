14bce5a8f1da7d901e9e728a0f999047deea0474
sagarbhadke08 <sagarbhadke08@gmail.com>
2022-12-02 10:51:52 +0530

    Added function for gender to show in table

--- form.js
+++ form.js
@@ -3,32 +3,39 @@ var entry = document.getElementById("submit-data-button");
 entry.addEventListener("click", showData);
 
 var row =1;
-function showData(){
-     var fname = document.getElementById("fname").value;
-     var lname = document.getElementById("lname").value;
-     var name = fname + ' ' + lname;
-     var strDob = document.getElementById("Dob").value;
-     var strStates = document.getElementById("states").value;
-     var strGender = document.getElementsByTagName('input');
+     function showData(){
+          var fname = document.getElementById("fname").value;
+          var lname = document.getElementById("lname").value;
+          var name = fname + ' ' + lname;
+          var strDob = document.getElementById("Dob").value;
+          var strStates = document.getElementById("states").value;
+          var strGender = document.getElementsByName('gender');
+          
+          var strGender_value;
+               for(i=0; i<strGender.length; i++){
+                    if(strGender[i].checked){
+                         strGender_value = strGender[i].value;
+                    }
+               }
 
-     if(!name || !strDob || !strStates || !strGender){
-          alert("please fill all the boxes");
-          return;
+                    if(!name || !strDob || !strStates || !strGender){
+                              alert("please fill all the boxes");
+                              return;
+                    }
+     
+               var display = document.getElementById("display");
+               var newRow = display.insertRow(row);
+
+               var cell1 = newRow.insertCell(0);
+               var cell2 = newRow.insertCell(1);
+               var cell3 = newRow.insertCell(2);
+               var cell4 = newRow.insertCell(3);
+               
+               cell1.innerText = name;
+               cell2.innerText = strDob;
+               cell3.innerText = strStates;
+               cell4.innerText = strGender_value;
+               row++;
+
      }
-    
-     var display = document.getElementById("display");
-     var newRow = display.insertRow(row);
-
-     var cell1 = newRow.insertCell(0);
-     var cell2 = newRow.insertCell(1);
-     var cell3 = newRow.insertCell(2);
-     var cell4 = newRow.insertCell(3);
-
-     cell1.innerText = name;
-     cell2.innerText = strDob;
-     cell3.innerText = strStates;
-     cell4.innerText = strGender;
-     row++;
-
-}
 
